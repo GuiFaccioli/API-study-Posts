865dfdc54cf9275185e9c5b2adfc25fb2c176eb6
Gui Faccioli <guifacciolic@gmail.com>
2026-03-18 10:31:46 -0300

    funcaoAtiva

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
 async function mostrarPost() {
-  const usarAPI = await fetch("https://jsonplaceholder.typicode.com/posts/1"); // já busca só o post 1
-  const post = await usarAPI.json();
+  const usarAPI = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const propriedadesJSON = await usarAPI.json();
+  const posts = propriedadesJSON.map((post) => ({
+    id: post.id,
+    title: post.title,
+    body: post.body,
+  }));
 
-  document.getElementById("idUsuario").innerText = post.id;
-  document.getElementById("titlePost").innerText = post.title;
-  document.getElementById("Post").innerText = post.body;
+  const container = document.querySelector(".containerCards");
+
+  posts.forEach((post) => {
+    const card = document.createElement("div");
+    card.classList.add("cardGeral");
+
+    card.innerHTML = `
+      <h1 class="idCard">${post.id}</h1>
+      <p class="titleCard">${post.title}</p>
+      <p class="bodyCard">${post.body}</p>
+    `;
+
+    container.appendChild(card);
+  });
 }
 
 mostrarPost();
